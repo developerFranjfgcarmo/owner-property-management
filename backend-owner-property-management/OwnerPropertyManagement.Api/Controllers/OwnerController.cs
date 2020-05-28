f898e7f88190a285c897bee6a4b3b5de7986f2e3
Francisco J Fernández <fjfernandez@salutic.es>
2020-05-29 00:51:21 +0200

    Create structure initial of the controllers.

---
+++ backend-owner-property-management/OwnerPropertyManagement.Api/Controllers/OwnerController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OwnerPropertyManagement.Api.Controllers
+{
+    [Route("api/Owner")]
+    [ApiController]
+    public class OwnerController : ControllerBase
+    {
+    }
+}
