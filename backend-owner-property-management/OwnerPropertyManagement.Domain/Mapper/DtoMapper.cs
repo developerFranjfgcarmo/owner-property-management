328fb6d2046f23fcea14321931d2bd05cd56e3ae
Francisco J Fernández <fjfernandez@salutic.es>
2020-05-29 00:34:32 +0200

    Configuration of Dtos Added utils for automapper

---
+++ backend-owner-property-management/OwnerPropertyManagement.Domain/Mapper/DtoMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using OwnerPropertyManagement.Data.Entities;
+using OwnerPropertyManagement.Domain.Dtos;
+
+namespace OwnerPropertyManagement.Domain.Mapper
+{
+    /// <summary>
+    ///     Configuration of Dtos
+    /// </summary>
+    public class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<OwnerDto, Owner>();
+            CreateMap<Owner, OwnerDto>();
+            CreateMap<Property, PropertyDto>();
+            CreateMap<PropertyDto, Property>();
+        }
+    }
+}
---
+++ backend-owner-property-management/OwnerPropertyManagement.Domain/Mapper/MappingExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace OwnerPropertyManagement.Domain.Mapper
+{
+    public static class MappingExtensions
+    {
+        /// <summary>
+        ///     Extension method to map this object type into destination type.
+        /// </summary>
+        /// <typeparam name="TDest"></typeparam>
+        /// <param name="src"></param>
+        /// <returns></returns>
+        public static TDest MapTo<TDest>(this object src)
+        {
+            return ObjectMapper.Mapper.Map<TDest>(src);
+        }
+    }
+}
