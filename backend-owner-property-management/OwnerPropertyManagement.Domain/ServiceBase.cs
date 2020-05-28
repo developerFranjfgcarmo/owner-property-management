4d9617e7995cf294d0feda01f082cdda83669c7d
Francisco J Fernández <fjfernandez@salutic.es>
2020-05-29 00:24:49 +0200

    Added the class ServiceBase

---
+++ backend-owner-property-management/OwnerPropertyManagement.Domain/ServiceBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using OwnerPropertyManagement.Data.Context;
+
+namespace OwnerPropertyManagement.Domain
+{
+    public class ServiceBase : IDisposable
+    {
+        public ServiceBase(IOwnerPropertyDbContext ownerPropertyDbContext)
+        {
+            OwnerPropertyDbContext = ownerPropertyDbContext;
+        }
+        public IOwnerPropertyDbContext OwnerPropertyDbContext { get; set; }
+
+        protected bool SaveChanges()
+        {
+            return OwnerPropertyDbContext.SaveChanges() > 0;
+        }
+
+        protected async Task<bool> SaveChangesAsync()
+        {
+            return await OwnerPropertyDbContext.SaveChangesAsync() > 0;
+        }
+
+        #region [Disposable]
+        ~ServiceBase()
+        {
+            Dispose(false);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+                OwnerPropertyDbContext.Dispose();
+        }
+
+        public void Dispose()
+        {
+            Dispose(false);
+        }
+        #endregion
+       
+    }
+}
