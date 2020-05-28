27aaab9d9498b71b8312a0dd127619bc072f1fa6
Francisco J Fernández <fjfernandez@salutic.es>
2020-05-29 00:29:17 +0200

    added initial dtos

---
+++ backend-owner-property-management/OwnerPropertyManagement.Domain/Dtos/OwnerDto.cs
@@ -0,0 +1,20 @@
+﻿namespace OwnerPropertyManagement.Domain.Dtos
+{
+    public class OwnerDto: AuditableDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Nick { get; set; }
+        public string PersonalPhoneNumber { get; set; }
+        public string ContactPhoneProperty { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string AboutMe { get; set; }
+        public bool Active { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
---
+++ backend-owner-property-management/OwnerPropertyManagement.Domain/Dtos/PropertyDto.cs
@@ -0,0 +1,26 @@
+﻿namespace OwnerPropertyManagement.Domain.Dtos
+{
+    public class PropertyDto:AuditableDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public short Sleeps { get; set; }
+        public short Bedrooms { get; set; }
+        public short Bathrooms { get; set; }
+        public bool PrivatePool { get; set; }
+        public bool HeatedPool { get; set; }
+        public bool Wifi { get; set; }
+        public bool Garden { get; set; }
+        public int LivingArea { get; set;}
+        public short DistanceAirport { get; set; }
+        public short DistanceBeach { get; set; }
+        public bool Active { get; set; }
+        public bool IsDeleted { get; set; }
+        public string LocalLeisure { get; set; }
+        public string LocalActivities { get; set; }
+        public int OwnerId { get; set; }
+
+
+    }
+}
