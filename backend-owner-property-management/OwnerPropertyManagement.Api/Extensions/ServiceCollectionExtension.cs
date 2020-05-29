65c95acf464bca043e006de8cfe14a2ab41e3985
Francisco J Fernández <fjfernandez@salutic.es>
2020-05-29 17:16:29 +0200

    add the dependency injection.

---
+++ backend-owner-property-management/OwnerPropertyManagement.Api/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OwnerPropertyManagement.Data.Context;
+using OwnerPropertyManagement.Domain.IServices;
+using OwnerPropertyManagement.Domain.Services;
+
+namespace OwnerPropertyManagement.Api.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddDataBase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OwnerPropertyDbContext>(c =>
+                c.UseSqlServer(configuration.GetConnectionString("OwnerPropertyDatabase")));
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IOwnerPropertyDbContext, OwnerPropertyDbContext>();
+            services.AddScoped<IOwnerService, OwnerService>();
+            services.AddScoped<IPropertyService, PropertyService>();
+            return services;
+        }
+    }
+}
--- backend-owner-property-management/OwnerPropertyManagement.Api/Startup.cs
+++ backend-owner-property-management/OwnerPropertyManagement.Api/Startup.cs
@@ -1,21 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OwnerPropertyManagement.Api.Extensions;
 
 namespace OwnerPropertyManagement.Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            //Dependency Injection
+            services.AddDataBase(Configuration).AddServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,13 +35,7 @@ namespace OwnerPropertyManagement.Api
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
