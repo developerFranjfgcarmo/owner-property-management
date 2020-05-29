14429f29e5386369f3a042a9eb78a4f1cd7d9cb3
Francisco J Fernández <fjfernandez@salutic.es>
2020-05-29 07:53:50 +0200

    Created the class to paged

---
+++ backend-owner-property-management/OwnerPropertyManagement.Domain/Dtos/PagedCollection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace OwnerPropertyManagement.Domain.Dtos
+{
+    public class PagedCollection<TDto>
+    {
+        public List<TDto> Items { get; set; }
+        public int Total { get; set; }
+    }
+}
