c4f090295d1be0359f4c61de03ac3229741745b9
Francisco J Fernández <fjfernandez@salutic.es>
2020-05-30 10:42:31 +0200

    Create home page

---
+++ src/OwnerPropertyManagement.App/App/Home/Controllers/homeController.js
@@ -0,0 +1,7 @@
+﻿"use strict";
+angular.module("ownerPropertyManagementApp").controller("homeController", [
+    "$scope", "$state", function ($scope, $state) {
+        var vm = this;
+
+    }
+]);
