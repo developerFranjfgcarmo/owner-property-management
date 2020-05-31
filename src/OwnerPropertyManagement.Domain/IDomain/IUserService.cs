f5d392e897d65f8456091d284f0d797fc8d4bcea
Francisco J Fernández <fjfernandez@salutic.es>
2020-05-31 13:53:40 +0200

    Authentication logic added

---
+++ src/OwnerPropertyManagement.Domain/IDomain/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using HealthcareManagement.Api.Auth.Entities;
+
+namespace HealthcareManagement.Api.Auth.Sevices
+{
+    public interface IUserService
+    { 
+        User Authenticate(string username, string password);
+        User GetById(int id);
+    }
+}
