fdce6866826744fb11c2e0942553f12008ffa24a
Francisco J Fernández <fjfernandez@salutic.es>
2020-06-01 23:16:42 +0200

    added contract/request

--- src/OwnerPropertyManagement.Api/Validator/OwnerValidator.cs
+++ src/OwnerPropertyManagement.Api/Validator/OwnerValidator.cs
@@ -5,5 +5,11 @@ namespace OwnerPropertyManagement.Api.Validator
 {
     public class OwnerValidator:AbstractValidator<OwnerDto>
     {
+        public OwnerValidator()
+        {
+            RuleFor(r => r.FirstName).NotEmpty().WithMessage("FirstName is required");
+            RuleFor(r => r.Surname).NotEmpty().WithMessage("FirstName is required");
+            RuleFor(r => r.PersonalPhoneNumber).NotEmpty().WithMessage("Personal phone number is required");
+        }
     }
 }
--- src/OwnerPropertyManagement.Api/Validator/PropertyValidator.cs
+++ src/OwnerPropertyManagement.Api/Validator/PropertyValidator.cs
@@ -7,10 +7,14 @@ using OwnerPropertyManagement.Contracts.Dtos;
 
 namespace OwnerPropertyManagement.Api.Validator
 {
-    public class PropertyValidator : AbstractValidator<OwnerDto>
+    public class PropertyValidator : AbstractValidator<PropertyDto>
     {
-        public PropertyValidator() {
-
+        public PropertyValidator()
+        {
+            RuleFor(r => r.OwnerId).NotEmpty().WithMessage("The owner is required");
+            RuleFor(r => r.Name).NotEmpty().WithMessage("The name is required");
+            RuleFor(r => r.TownId).NotEqual(0).WithMessage("The town is required");
+            RuleFor(r => r.Facilities).Must(m=>!m.Any()).WithMessage("You must select some facility");
         }
     }
 }
