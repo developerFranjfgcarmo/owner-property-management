28fabd1e0ef93eb4ba26f8cf3b916366c2e1cea2
Francisco J Fernández <fjfernandez@salutic.es>
2020-06-02 08:07:42 +0200

    The acction of removing is added to owner

--- src/OwnerPropertyManagement.App/App/Owner/Controller/ownerEditController.js
+++ src/OwnerPropertyManagement.App/App/Owner/Controller/ownerEditController.js
@@ -6,24 +6,25 @@ angular
     'id',
     "$uibModalInstance",
     "toaster",
-    function ($scope, ownerService, id, $uibModalInstance,toaster) {
+    function ($scope, ownerService, id, $uibModalInstance, toaster) {
       var vm = this;
       vm.id = id;
-      vm.owner = {};      
+      vm.owner = {};
       vm.currentModel = {};
-      vm.errors=undefined;
+      vm.errors = undefined;
 
       vm.get = get;
       vm.dismiss = dismiss;
-      vm.modelIsValid = modelIsValid; 
+      vm.modelIsValid = modelIsValid;
       vm.save = save;
+      
       init();
 
-      function init () {       
+      function init() {
         get(vm.id);
       }
 
-      function get (id) {
+      function get(id) {
         if (id === 0) {
           return;
         }
@@ -36,15 +37,16 @@ angular
             //todo:show the error
           } */
         )
-      }    
+      }
 
       function dismiss() {
         $uibModalInstance.dismiss();
       }
 
       function modelIsValid() {
-          return vm.frm.$valid && !angular.equals(vm.currentModel, vm.owner);
-      } 
+        return vm.frm.$valid && !angular.equals(vm.currentModel, vm.owner);
+      }
+
       function save() {
         if (vm.id === 0) {
           ownerService.add(vm.owner).then(function (response) {
@@ -53,19 +55,18 @@ angular
             toaster.pop("success", "Owner created successfully");
             $uibModalInstance.close();
           }, function (errors) {
-              vm.errors = errors;
+            vm.errors = errors;
           });
-      } else {
-        ownerService.update(vm.owner).then(function (response) {
-              vm.owner = response;
-              vm.currentModel = angular.copy(response);
-              toaster.pop("success", "Owner updated successfully");
-              $uibModalInstance.close();
+        } else {
+          ownerService.update(vm.owner).then(function (response) {
+            vm.owner = response;
+            vm.currentModel = angular.copy(response);
+            toaster.pop("success", "Owner updated successfully");
+            $uibModalInstance.close();
           }, function (errors) {
-              vm.errors = errors;
+            vm.errors = errors;
           });
-      }      
-      } 
-      
+        }
+      }
     }
   ])
--- src/OwnerPropertyManagement.App/App/Owner/Controller/ownerListController.js
+++ src/OwnerPropertyManagement.App/App/Owner/Controller/ownerListController.js
@@ -1,68 +1,83 @@
 angular.module('ownerPropertyManagementApp').controller('ownerListController', [
-    '$scope',
-    '$state',
-    'ownerService',
-    '$uibModal',
-    'masterTablesService',
-    function ($scope, $state, ownerService, $uibModal, masterTablesService) {
-      const vm = this;
-      vm.total = 0;
-      vm.owners = [];
-      vm.filter = { take: 5, page: 0 };
-      vm.attributes = [      
-        { displayName: 'FirstName', attribute: 'firstName' },
-        { displayName: 'Surname', attribute: 'surname' },
-        { displayName: 'Nick', attribute: 'nick' },
-        { displayName: 'Personal Phone', attribute: 'personalPhoneNumber' },
-        { displayName: 'City', attribute: 'city' },
-        { displayName: 'Country', attribute: 'country' },
-        { displayName: 'Edit', attribute: 'action' }
-      ];
-      vm.getAll = getAll;
-      vm.addOrUpdate = addOrUpdate;
-  
-      init()
-  
-      function init () {
-        masterTablesService.getOwnerNameList().then(function (res) {
-          vm.owners = res;
-        })
-        getAll();
-      }
-  
-      function getAll () {
-        ownerService.getAll(vm.filter).then(
-          function (response) {
-            vm.owners = response.items;
-            vm.total = response.total;
-            vm.numOfPages = [];
-            for (let i = 0; i * vm.filter.take < vm.total; i++) {
-              vm.numOfPages.push(i);
-            }
+  '$scope',
+  '$state',
+  'ownerService',
+  '$uibModal',
+  'masterTablesService',
+  "toaster",
+  function ($scope, $state, ownerService, $uibModal, masterTablesService, toaster) {
+    const vm = this;
+    vm.total = 0;
+    vm.owners = [];
+    vm.filter = { take: 5, page: 0 };
+    vm.attributes = [
+      { displayName: 'FirstName', attribute: 'firstName' },
+      { displayName: 'Surname', attribute: 'surname' },
+      { displayName: 'Nick', attribute: 'nick' },
+      { displayName: 'Personal Phone', attribute: 'personalPhoneNumber' },
+      { displayName: 'City', attribute: 'city' },
+      { displayName: 'Country', attribute: 'country' },
+      { displayName: 'Acctions', attribute: 'action' }
+    ];
+
+    vm.getAll = getAll;
+    vm.addOrUpdate = addOrUpdate;
+    vm.remove = remove;
+
+    init()
+
+    function init() {
+      masterTablesService.getOwnerNameList().then(function (res) {
+        vm.owners = res;
+      })
+      getAll();
+    }
+
+    function getAll() {
+      ownerService.getAll(vm.filter).then(
+        function (response) {
+          vm.owners = response.items;
+          vm.total = response.total;
+          vm.numOfPages = [];
+          for (let i = 0; i * vm.filter.take < vm.total; i++) {
+            vm.numOfPages.push(i);
+          }
+        },
+        function (error) {
+          //todo:show the error
+        }
+      )
+    }
+    function addOrUpdate(id) {
+      $uibModal.open({
+        templateUrl: "/App/Owner/Views/ownerEdit.html",
+        controller: "ownerEditController",
+        replace: true,
+        controllerAs: "ownerEditCtrl",
+        resolve: {
+          ownerService: function () {
+            return ownerService;
           },
-          function (error) {
-            //todo:show the error
+          id: function () {
+            return id;
           }
-        )
-      }
-      function addOrUpdate(id){
-        $uibModal.open({
-          templateUrl: "/App/Owner/Views/ownerEdit.html",
-          controller: "ownerEditController",
-          replace: true,
-          controllerAs: "ownerEditCtrl",           
-          resolve: {            
-              ownerService: function() {
-                return ownerService;
-            },
-            id: function () {
-              return id;
-          }
-          },        
-      }).result.then(function() {
-          getAll();
+        },
+      }).result.then(function () {
+        getAll();
       });
+    }
+    function remove(id) {
+      if (vm.id !== 0) {
+        ownerService.remove(id).then(function (response) {
+          toaster.pop("success", "Owner removed successfully");
+          $uibModalInstance.close();
+        }, function (errors) {
+          if(!errors){
+            errors="Owner could not be removed";
+          }
+          toaster.pop("error", errors);
+        });
       }
     }
-  ])
-  
+  }
+])
